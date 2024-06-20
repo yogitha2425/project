75396720b6e20b191c28cb4b5d5850c20a08f08c
yogitha2425 <98255163+yogitha2425@users.noreply.github.com>
2024-06-20 19:30:37 +0530

    Add files via upload

--- convert.py
+++ convert.py
@@ -1,1 +1,11 @@
+from moviepy.editor import VideoFileClip
+import os
+import tqdm
 
+def convert_mpg_to_mp4(input_file, output_file):
+  clip = VideoFileClip(input_file)
+  clip.write_videofile(output_file)
+
+for i in tqdm.tqdm(os.listdir('data/videos')):
+  output_file = 'data/final_videos/' + i.split('.')[0] + '.mp4'
+  convert_mpg_to_mp4(f'data/videos/{i}', output_file)
